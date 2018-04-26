2d669b5991a44491eee6e689c4433c63757486d2
Rithik Yelisetty <rithik@gmail.com>
2018-04-25 23:18:17 -0400

    Registration Views

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 import os
-from models import User
+from models import Student, Course, School, College, Transcript
 import settings
 import sys
 from database import db_session
@@ -15,10 +15,83 @@ app.config['SQLALCHEMY_DATABASE_URI'] = settings.DB_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = settings.TRACK_MODIFICATIONS
 app.secret_key = settings.SECRET_KEY
 
+def log(msg):
+    if settings.PRODUCTION:
+        pass
+    else:
+        print(msg, file=sys.stderr)
+
 @app.route('/')
 def home_page():
     return "Home Page"
 
+@app.route('/school/<school_id>/register/student')
+def create_student(school_id): # Students must be registered through their school
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        password = request.form["password"]
+        confirm_password = request.form["confirm_password"]
+        if not email.index("@") > 0 and not email.index(".") > email.index("@"):
+            return redirect(url_for('.home_page'))
+        if not confirm_password == password:
+            return redirect(url_for('.home_page'))
+        u = Student.query.filter_by(email=email).count()
+        if u == 0:
+            new_student = Student(first_name, last_name, email, password, int(school_id))
+            db_session.add(new_student)
+            db_session.commit()
+            log('Student Created')
+            return redirect(url_for('.home_page'))
+        return redirect(url_for('.home_page'))
+
+@app.route('/register/school')
+def create_school():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        address = request.form["address"]
+        password = request.form["password"]
+        confirm_password = request.form["confirm_password"]
+        if not confirm_password == password:
+            return redirect(url_for('.home_page'))
+        new_school = School(name, email, address, password)
+        db_session.add(new_school)
+        db_session.commit()
+        log('School Created')
+        return redirect(url_for('.home_page'))
+
+@app.route('/register/college')
+def create_college():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+        confirm_password = request.form["confirm_password"]
+        if not confirm_password == password:
+            return redirect(url_for('.home_page'))
+        new_college = College(name, email, password)
+        db_session.add(new_college)
+        db_session.commit()
+        log('College Created')
+        return redirect(url_for('.home_page'))
+
+@app.route('/school/<school_id>/student/<student_id>')
+def create_college():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+        confirm_password = request.form["confirm_password"]
+        if not confirm_password == password:
+            return redirect(url_for('.home_page'))
+        new_college = College(name, email, password)
+        db_session.add(new_college)
+        db_session.commit()
+        log('College Created')
+        return redirect(url_for('.home_page'))
+
+
 if __name__ == '__main__':
     app.run()
-
--- models.py
+++ models.py
@@ -30,36 +30,44 @@ class Student(Base):
 
 class Course(Base):
     __tablename__ = 'courses'
-    
+
     id = Column(Integer, primary_key=True)
     name = Column(String(length=100))
     grade = Column(String(length=5))
     credits = Column(Float)
+    year = Column(Integer)
 
-    def __init__(self, name, grade, credits):
+    def __init__(self, name, grade, credits, year):
         self.name = name
         self.grade = grade
         self.credits = credits
+        self.year = year
 
 class School(Base):
     __tablename__ = 'schools'
-    
+
     id = Column(Integer, primary_key=True)
     name = Column(String(length=500))
+    email = Column(String(length=120), unique=True)
     address = Column(String(length=10000))
-    
-    def __init__(self, name, address):
+    password = Column(String(length=50))
+
+    def __init__(self, name, email, address, password):
         self.name = name
         self.address = address
-  
+        self.password = password
+
 class College(Base):
     __tablename__ = 'colleges'
-    
+
     id = Column(Integer, primary_key=True)
     name = Column(String(length=500))
+    email = Column(String(length=120), unique=True)
+    password = Column(String(length=50))
 
-    def __init__(self, name):
+    def __init__(self, name, email, password):
         self.name = name
+        self.password
 
 class Transcript(Base):
     __tablename__ = 'transcripts'
@@ -68,5 +76,6 @@ class Transcript(Base):
     courses = relationship('Course', backref='transcript', lazy='dynamic')
     student = relationship('Student', backref='transcript', lazy='dynamic')
     school = relationship('School', backref='transcripts', lazy='dynamic')
-    colleges = relationship('College', backref='transcripts', lazy='dynamic')     
+    colleges = relationship('College', backref='transcripts', lazy='dynamic')
 
+    # Add college by doing: transcript.colleges.append(c)
